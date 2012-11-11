3765d5ace03e315de6db6807e4fe49ba1b921d9a
Tim <timothy.fite@gmail.com>
2012-11-11 17:26:03 -0600

    Testing JSON services

--- src/main/java/com/metispro/web/rest/MemberResourceRESTService.java
+++ src/main/java/com/metispro/web/rest/MemberResourceRESTService.java
@@ -1,5 +1,6 @@
 package com.metispro.web.rest;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
@@ -10,37 +11,76 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.metispro.web.model.Member;
 
 /**
  * JAX-RS Example
  * 
- * This class produces a RESTful service to read the contents of the members table.
+ * This class produces a RESTful service to read the contents of the members
+ * table.
  */
 @Path("/members")
 @RequestScoped
 public class MemberResourceRESTService {
-   @Inject
-   private EntityManager em;
+	@Inject
+	private EntityManager em;
 
-   @GET
-   @Produces("text/xml")
-   public List<Member> listAllMembers() {
-      // Use @SupressWarnings to force IDE to ignore warnings about "genericizing" the results of
-      // this query
-      @SuppressWarnings("unchecked")
-      // We recommend centralizing inline queries such as this one into @NamedQuery annotations on
-      // the @Entity class
-      // as described in the named query blueprint:
-      // https://blueprints.dev.java.net/bpcatalog/ee5/persistence/namedquery.html
-      final List<Member> results = em.createQuery("select m from Member m order by m.name").getResultList();
-      return results;
-   }
+	@GET
+	@Produces("text/xml")
+	public List<Member> listAllMembers() {
+		// Use @SupressWarnings to force IDE to ignore warnings about
+		// "genericizing" the results of
+		// this query
+		@SuppressWarnings("unchecked")
+		// We recommend centralizing inline queries such as this one into
+		// @NamedQuery annotations on
+		// the @Entity class
+		// as described in the named query blueprint:
+		// https://blueprints.dev.java.net/bpcatalog/ee5/persistence/namedquery.html
+		final List<Member> results = em.createQuery(
+				"select m from Member m order by m.name").getResultList();
+		return results;
+	}
 
-   @GET
-   @Path("/{id:[0-9][0-9]*}")
-   @Produces("text/xml")
-   public Member lookupMemberById(@PathParam("id") long id) {
-      return em.find(Member.class, id);
-   }
+	@GET
+	@Path("/{id:[0-9][0-9]*}")
+	@Produces("text/xml")
+	public Member lookupMemberById(@PathParam("id") long id) {
+		return em.find(Member.class, id);
+	}
+
+	@GET
+	@Path("/json/{id:[0-9][0-9]*}")
+	@Produces("text/plain")
+	public String getJSONMemberByID(@PathParam("id") long id) {
+		Member member = em.find(Member.class, id);
+
+		JSONObject jsonObj = new JSONObject(member);
+
+		return jsonObj.toString();
+	}
+
+	@GET
+	@Path("/json")
+	@Produces("text/plain")
+	public String listAllJSONMembers() {
+		@SuppressWarnings("unchecked")
+		final List<Member> results = em.createQuery(
+				"select m from Member m order by m.name").getResultList();
+
+		System.out.println("Found " + results.size() + " results");
+
+		JSONArray jsonArr = new JSONArray();
+
+		for (Member member : results) {
+			JSONObject jsonObj = new JSONObject(member);
+
+			jsonArr.put(jsonObj);
+		}
+
+		return jsonArr.toString();
+	}
 }
